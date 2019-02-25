5d25cc726d412074f7330f58bab746fec4836ce6
guifeng <guifengleaf@gmail.com>
2019-02-25 20:14:11 +0800

    6

---
+++ mycode/src/main/java/binarySearch/MySqrt.java
@@ -0,0 +1,16 @@
+package binarySearch;
+
+/*
+69. Sqrt(x)
+Compute and return the square root of x,
+where x is guaranteed to be a non-negative integer.
+Since the return type is an integer, the decimal digits are truncated
+and only the integer part of the result is returned.
+https://leetcode.com/problems/sqrtx/
+ */
+public class MySqrt {
+  public int mySqrt(int x) {
+
+    return -1;
+  }
+}
